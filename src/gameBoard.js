b1b33cda938a5ef327bf72eb14cbff85688a2ac4
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-02-06 14:47:09 +0500

    Add functions and unit tests to receive attack and check all ships sunk.

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -2,8 +2,13 @@ import Ship from './ship';
 
 const GameBoard = () => {
   const ships = [];
+  const missedCoordinates = [];
 
   const placeShip = (type, length, coordinates) => {
+    if (Object.keys(coordinates).length !== length) {
+      throw new TypeError('Length and num of coordinates mismatch');
+    }
+
     const ship = new Ship(type, length);
     ship.coordinates.push(...coordinates);
     ships.push(ship);
@@ -11,7 +16,39 @@ const GameBoard = () => {
     return ship;
   };
 
-  return { placeShip };
+  const receiveAttack = (coordinate) => {
+    if (!(coordinate.x && coordinate.y)) {
+      throw new TypeError('Bad input invalid coordinate');
+    }
+
+    let coordinateIndex;
+
+    const shipIndex = ships.findIndex((ship) => {
+      coordinateIndex = ship.coordinates.findIndex((ele) => {
+        if (ele.x === coordinate.x && ele.y === coordinate.y) {
+          return true;
+        }
+        return false;
+      });
+
+      if (coordinateIndex !== -1) {
+        ship.hit(coordinate);
+        return true;
+      }
+      return false;
+    });
+
+    if (shipIndex !== -1) {
+      console.log(`Ship: ${shipIndex}, Coordinate Index: ${coordinateIndex}`);
+      return true;
+    }
+    missedCoordinates.push(coordinate);
+    return missedCoordinates;
+  };
+
+  const allShipsSunk = () => ships.every((ship) => ship.isSunk());
+
+  return { placeShip, receiveAttack, allShipsSunk };
 };
 
 export default GameBoard;
--- tests/gameBoard.test.js
+++ tests/gameBoard.test.js
@@ -1,9 +1,51 @@
 import GameBoard from '../src/gameBoard';
 import Ship from '../src/ship';
 
+let gameBoard;
+
+beforeAll(() => {
+  gameBoard = GameBoard();
+});
+
 test('placeShip method places a ship on the gameboard', () => {
-  const gameBoard = GameBoard();
   const ship = gameBoard.placeShip('submarine', 3, [{ x: 1, y: 1 }, { x: 1, y: 2 }, { x: 1, y: 3 }]);
   expect(ship).toBeInstanceOf(Ship);
   console.log(ship);
+});
+
+test('placeship method throws error if length and num of coordinates are not consistent', () => {
+  expect.assertions(1);
+  try {
+    gameBoard.placeShip('submarine', 2, [{ x: 2, y: 1 }, { x: 2, y: 2 }, { x: 2, y: 3 }]);
+  } catch (e) {
+    expect(e).toBeInstanceOf(TypeError);
+  }
+});
+
+test('receiveAttack method expects to receive a coordinate with x and y values', () => {
+  expect.assertions(1);
+  try {
+    gameBoard.receiveAttack({ x: 1 });
+  } catch (e) {
+    expect(e).toBeInstanceOf(TypeError);
+  }
+});
+
+test('receiveAttack method returns true when a ship is hit', () => {
+  expect(gameBoard.receiveAttack({ x: 1, y: 2 })).toBe(true);
+});
+
+test('receiveAttack method updates missedCoordinates when no hit occurs', () => {
+  const missedCoordinates = gameBoard.receiveAttack({ x: 2, y: 2 });
+  expect(missedCoordinates).toEqual([{ x: 2, y: 2 }]);
+});
+
+test('allShipSunk method returns false when all ships have not sunk', () => {
+  expect(gameBoard.allShipsSunk()).toBe(false);
+});
+
+test('allShipsSunk method returns true when all ships have sunk', () => {
+  gameBoard.receiveAttack({ x: 1, y: 1 });
+  gameBoard.receiveAttack({ x: 1, y: 3 });
+  expect(gameBoard.allShipsSunk()).toBe(true);
 });
